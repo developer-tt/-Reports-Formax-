7321d209556250b5f59791f4da3c5aa97f81f234
Alan Orozco <alanorozco07@gmail.com>
2020-03-31 13:58:18 -0700

    Added rounded corners and lottie animation to loading Dialog

--- TimeTrackerBIXF/TimeTrackerBIXF.Android/obj/Debug/90/android/environment.x86_64.s
+++ TimeTrackerBIXF/TimeTrackerBIXF.Android/obj/Debug/90/android/environment.x86_64.s
@@ -69,7 +69,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",@progbits,1
 	.type	.L.str.8, @object
 .L.str.8:
-	.asciz	"8f5226f0-1ab9-4d43-a986-9f750440aef3"
+	.asciz	"92bc632e-6315-4d97-8432-a85864d73421"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",@progbits,1
 	.type	.L.str.9, @object
